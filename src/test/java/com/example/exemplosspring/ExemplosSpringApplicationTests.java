1c7f847a31a77610740a3cff94f0f406a40b5385
Ramon Tanure <ramon_tanure_carvalho@hotmail.com>
2022-12-06 18:00:42 -0300

    Primeiro commit

---
+++ src/test/java/com/example/exemplosspring/ExemplosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exemplosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
